0a3e55326f4402860cff74fc53f40a3023f56e82
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-05-09 01:25:34 +0200

    Create paginated api for beers (#1)
    * Create backbones for paginated api
    
    * Send user to first or last page if page is out of bounds

--- src/biergarten.ts
+++ src/biergarten.ts
@@ -1,3 +1,6 @@
+// Playground file, just for me to play around with the jsons and see
+// Called it biergarted because biergartens are like adult beer playgorunds
+
 import { wipeAndWriteToFile } from './utils/filehandling.ts'
 import winningBeers from '../data/winning-beers.json' with { type: 'json' }
 
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
+import winningBeers from "../data/winning-beers.json" with { type: "json" };
 
+Deno.serve((req) => {
+	const url = new URL(req.url);
+	const path = url.pathname;
+
+	if (path === "/beers") {
+		let page = parseInt(url.searchParams.get("page") || "1")
+		const perPage = parseInt(url.searchParams.get("perPage") || "50")
+
+		if (isNaN(page) || isNaN(perPage)) {
+			return new Response("Invalid page or perPage parameter", { status: 400 });
+		}
+
+		if (page < 1) {
+			page = 1
+		}
+
+		let start = (page - 1) * perPage
+		let end = start + perPage
+
+		if (end > winningBeers.length) {
+			end = winningBeers.length
+			start = end - perPage < start ? start : end - perPage
+		}
+
+		const paginatedBeers = winningBeers.slice(start, end)
+
+		return new Response(JSON.stringify(paginatedBeers), {
+			headers: { "Content-Type": "application/json" },
+		});
+	}
+
+	return new Response(null, { status: 404 });
+})
